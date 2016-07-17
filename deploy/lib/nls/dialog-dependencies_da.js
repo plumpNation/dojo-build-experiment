74cd7f93f72f263ce045c545c4bff692ed2d5b11
Gavin King <gavin.king@itslearning.com>
2016-07-17 19:44:19 +0200

    Removed deploy folder from repo

--- deploy/lib/nls/dialog-dependencies_da.js
+++
@@ -1,5 +0,0 @@
-define('lib/nls/dialog-dependencies_da',{
-'dijit/nls/loading':{"loadingState":"Indlæser...","errorState":"Der er opstået en fejl","_localized":{}}
-,
-'dijit/nls/common':{"buttonOk":"OK","buttonCancel":"Annullér","buttonSave":"Gem","itemClose":"Luk","_localized":{}}
-});
--- deploy/lib/nls/dialog-dependencies_tr.js
+++
@@ -1,5 +0,0 @@
-define('lib/nls/dialog-dependencies_tr',{
-'dijit/nls/loading':{"loadingState":"Yükleniyor...","errorState":"Üzgünüz, bir hata oluştu","_localized":{}}
-,
-'dijit/nls/common':{"buttonOk":"Tamam","buttonCancel":"İptal","buttonSave":"Kaydet","itemClose":"Kapat","_localized":{}}
-});
